2529753fbc5320ecb4e8ea7a7713e74ac92e13f2
Allan Wollaber <Allan.Wollaber@ll.mit.edu>
2023-07-08 21:42:56 -0400

    Added the header back to utils.py

--- src/equine/utils.py
+++ src/equine/utils.py
@@ -1,3 +1,7 @@
+# Copyright 2023, MASSACHUSETTS INSTITUTE OF TECHNOLOGY
+# Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
+# SPDX-License-Identifier: MIT
+
 from typing import Any, List, Union, Tuple
 import icontract
 import torch

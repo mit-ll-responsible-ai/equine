04be69b9f66e1be9fa54c2b046051b128faa174e
jholodnak <john.holodnak@ll.mit.edu>
2025-11-10 14:11:47 -0500

    isort error

--- src/equine/equine_gp.py
+++ src/equine/equine_gp.py
@@ -2,7 +2,7 @@
 # Subject to FAR 52.227-11 – Patent Rights – Ownership by the Contractor (May 2014).
 # SPDX-License-Identifier: MIT
 
-from typing import Any, Optional, Union, Protocol, runtime_checkable
+from typing import Any, Optional, Protocol, Union, runtime_checkable
 
 import icontract
 import io
